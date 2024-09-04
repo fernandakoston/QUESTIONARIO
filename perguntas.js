c86397eff4383ba9a8065b53d939b5e779bd1ebb
Fernanda <106741909+fernandakoston@users.noreply.github.com>
2024-09-04 19:57:11 -0300

    Create perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,115 @@
+export const perguntas = [
+    {
+        enunciado: "Como você prefere estudar para uma prova importante??",
+        alternativas: [
+            {
+                texto: "A) Resolvendo exercícios e simulados",
+                afirmacao: [
+                    "Seu foco é na prática. Prefere resolver exercícios e aplicar o conhecimento adquirido diretamente. Sente-se confiante quando consegue explicar o conteúdo."
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "B) Revisando anotações e resumos",
+                afirmacao: [
+                    "Você gosta de estudar conforme sua motivação e estado de espírito. Prefere técnicas mais dinâmicas e busca variedade nos materiais para manter o interesse."
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual é o seu ambiente de estudo ideal?",
+        alternativas: [
+            {
+                texto: "A) Biblioteca silenciosa",
+                afirmacao: [
+                    " Você é focado e organizado. Prefere planejar seus estudos com antecedência e segue um cronograma rigoroso para garantir que cobriu tudo antes da prova.",
+    
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "B) Em casa, no seu quarto",
+                afirmacao: [
+                    "Você adora estudar em grupo e discutir ideias. Para você, aprender é mais eficiente quando compartilhado, e você se motiva ao trocar conhecimento com outros."
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: " O que você faz quando não entende um assunto?",
+        alternativas: [
+            {
+                texto: "A)Pergunto para colegas ou amigos",
+                afirmacao: [
+                    "Você gosta de estudar conforme sua motivação e estado de espírito. Prefere técnicas mais dinâmicas e busca variedade nos materiais para manter o interesse."
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "b)Estudo sozinho até entender",
+                afirmacao: [
+                    "Seu foco é na prática. Prefere resolver exercícios e aplicar o conhecimento adquirido diretamente. Sente-se confiante quando consegue explicar o conteúdo."
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual ferramenta você usa mais para estudar?",
+        alternativas: [
+            {
+                texto: "a) Aplicativos e plataformas de estudo",
+                afirmacao: [
+                    "Você adora estudar em grupo e discutir ideias. Para você, aprender é mais eficiente quando compartilhado, e você se motiva ao trocar conhecimento com outros."
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "b)Resumos e fichas de estudo",
+                afirmacao: [
+                    " Você é focado e organizado. Prefere planejar seus estudos com antecedência e segue um cronograma rigoroso para garantir que cobriu tudo antes da prova."
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: " Como você lida com o tempo de estudo?",
+        alternativas: [
+            {
+                texto: "A) Crio um cronograma detalhado",
+                afirmacao: [
+                    "Seu foco é na prática. Prefere resolver exercícios e aplicar o conhecimento adquirido diretamente. Sente-se confiante quando consegue explicar o conteúdo."
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "B) Estudo de acordo com minha motivação do dia",
+                afirmacao: [
+                    "Você gosta de estudar conforme sua motivação e estado de espírito. Prefere técnicas mais dinâmicas e busca variedade nos materiais para manter o interesse."
+                ],
+                proxima: 8,
+            },
+        ]
+    },
+    {
+        enunciado: " Como você lida com a pressão dos estudos?",
+        alternativas: [
+            {
+                texto: "A) Uso técnicas de relaxamento para acalmar",
+                afirmacao: [
+                    " Você é focado e organizado. Prefere planejar seus estudos com antecedência e segue um cronograma rigoroso para garantir que cobriu tudo antes da prova."
+                ],
+            },
+            {
+                texto: "B) Foco no conteúdo e tento não pensar na pressão",
+                afirmacao: [
+                    "Você adora estudar em grupo e discutir ideias. Para você, aprender é mais eficiente quando compartilhado, e você se motiva ao trocar conhecimento com outros."
+                ],
+            },
+        ]
+    },
+];
